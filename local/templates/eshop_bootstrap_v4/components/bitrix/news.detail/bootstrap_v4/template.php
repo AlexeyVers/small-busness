c7197c269e16970057bb51c6e3a8588ef4f9820d
AlexeyVers <juk50075@gmail.com>
2022-12-20 16:14:23 +0300

    102170 | - проверка ссылки, исправлена ошибка

--- local/templates/eshop_bootstrap_v4/components/bitrix/news.detail/bootstrap_v4/template.php
+++ local/templates/eshop_bootstrap_v4/components/bitrix/news.detail/bootstrap_v4/template.php
@@ -5,7 +5,7 @@
 <div class="news-detail">
     <?= $arResult['DETAIL_TEXT']; ?>
     <? $arProps = $arResult['DISPLAY_PROPERTIES'] ?>
-    <? if (arProps['DATE_CREATE']): ?>
+    <? if (arProps['DATE_CREATE']['VALUE']): ?>
         <div class="news-list-view news-list-post-params">
             <span class="news-list-param"><?= $arProps['DATE_CREATE']['NAME'] ?>:</span>
             <span class="news-list-value"><?= $arProps['DATE_CREATE']['VALUE'] ?></span>
--- local/templates/eshop_bootstrap_v4/components/bitrix/news.list/bootstrap_v4/template.php
+++ local/templates/eshop_bootstrap_v4/components/bitrix/news.list/bootstrap_v4/template.php
@@ -11,32 +11,21 @@
                     <img src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" class="card-img-top"/>
                 <? endif; ?>
                 <div class="card-body">
-                    <div class="card-title">
-                        <? if ($arItem['DETAIL_PAGE_URL']): ?>
-                            <a href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a>
-                        <? else: ?>
-                            <div><?= $arItem['NAME'] ?></div>
-                        <? endif; ?>
-                    </div>
+                    <div class="card-title"><a
+                                href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a></div>
                     <? if ($arItem['PREVIEW_TEXT']): ?>
                         <p class="card-text"><?= $arItem['PREVIEW_TEXT']; ?></p>
                     <? endif; ?>
-                    <? if ($arProps['DATE_CREATE']): ?>
+                    <? if ($arProps['DATE_CREATE']['VALUE']): ?>
                         <div class="news-list-view news-list-post-params">
                             <span class="news-list-param"><?= $arProps['DATE_CREATE']['NAME'] ?>:</span>
                             <span class="news-list-value"><?= $arProps['DATE_CREATE']['VALUE'] ?></span>
                         </div>
                     <? endif; ?>
                     <div class="news-list-more">
-                        <? if ($arItem['DETAIL_PAGE_URL']): ?>
-                            <a class="btn btn-primary btn-sm" href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= GetMessage(
-                                    'CT_BNL_GOTO_DETAIL'
-                                ) ?></a>
-                        <? else: ?>
-                            <div class="btn btn-primary btn-sm"><?= GetMessage(
-                                    'CT_BNL_GOTO_DETAIL'
-                                ) ?></div>
-                        <? endif; ?>
+                        <a class="btn btn-primary btn-sm" href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= GetMessage(
+                                'CT_BNL_GOTO_DETAIL'
+                            ) ?></a>
                     </div>
                 </div>
             </div>
