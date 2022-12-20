911586cd11f19d8152a11f5f15fe3f4a887659fd
AlexeyVers <juk50075@gmail.com>
2022-12-20 15:34:23 +0300

    102170 | Проверка для текстов, картинок и ссылок

--- local/templates/eshop_bootstrap_v4/components/bitrix/news.detail/bootstrap_v4/template.php
+++ local/templates/eshop_bootstrap_v4/components/bitrix/news.detail/bootstrap_v4/template.php
@@ -4,11 +4,13 @@
 ?>
 <div class="news-detail">
     <?= $arResult['DETAIL_TEXT']; ?>
-    <? $arProps = $arResult['DISPLAY_PROPERTIES']; ?>
-    <div class="news-list-view news-list-post-params">
-        <span class="news-list-param"><?= $arProps['DATE_CREATE']['NAME'] ?>:</span>
-        <span class="news-list-value"><?= $arProps['DATE_CREATE']['VALUE'] ?></span>
-    </div>
+    <? $arProps = $arResult['DISPLAY_PROPERTIES'] ?>
+    <? if (arProps['DATE_CREATE']): ?>
+        <div class="news-list-view news-list-post-params">
+            <span class="news-list-param"><?= $arProps['DATE_CREATE']['NAME'] ?>:</span>
+            <span class="news-list-value"><?= $arProps['DATE_CREATE']['VALUE'] ?></span>
+        </div>
+    <? endif; ?>
 </div>
 <div class="right">
     <script src="https://yastatic.net/share2/share.js" defer></script>
--- local/templates/eshop_bootstrap_v4/components/bitrix/news.list/bootstrap_v4/template.php
+++ local/templates/eshop_bootstrap_v4/components/bitrix/news.list/bootstrap_v4/template.php
@@ -5,21 +5,38 @@
 <div class="col">
     <div class="row">
         <? foreach ($arResult['ITEMS'] as $arItem): ?>
-        <? $arProps = $arItem['DISPLAY_PROPERTIES']; ?>
+            <? $arProps = $arItem['DISPLAY_PROPERTIES'] ?>
             <div class="card normal-width">
-                <img src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" class="card-img-top"/>
+                <? if ($arItem['PREVIEW_PICTURE']): ?>
+                    <img src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" class="card-img-top"/>
+                <? endif; ?>
                 <div class="card-body">
-                    <div class="card-title"><a
-                                href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a></div>
-                    <p class="card-text"><?= $arItem['PREVIEW_TEXT']; ?></p>
-                    <div class="news-list-view news-list-post-params">
-                        <span class="news-list-param"><?= $arProps['DATE_CREATE']['NAME'] ?>:</span>
-                        <span class="news-list-value"><?= $arProps['DATE_CREATE']['VALUE'] ?></span>
+                    <div class="card-title">
+                        <? if ($arItem['DETAIL_PAGE_URL']): ?>
+                            <a href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a>
+                        <? else: ?>
+                            <div><?= $arItem['NAME'] ?></div>
+                        <? endif; ?>
                     </div>
+                    <? if ($arItem['PREVIEW_TEXT']): ?>
+                        <p class="card-text"><?= $arItem['PREVIEW_TEXT']; ?></p>
+                    <? endif; ?>
+                    <? if ($arProps['DATE_CREATE']): ?>
+                        <div class="news-list-view news-list-post-params">
+                            <span class="news-list-param"><?= $arProps['DATE_CREATE']['NAME'] ?>:</span>
+                            <span class="news-list-value"><?= $arProps['DATE_CREATE']['VALUE'] ?></span>
+                        </div>
+                    <? endif; ?>
                     <div class="news-list-more">
-                        <a class="btn btn-primary btn-sm" href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= GetMessage(
-                                'CT_BNL_GOTO_DETAIL'
-                            ) ?></a>
+                        <? if ($arItem['DETAIL_PAGE_URL']): ?>
+                            <a class="btn btn-primary btn-sm" href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= GetMessage(
+                                    'CT_BNL_GOTO_DETAIL'
+                                ) ?></a>
+                        <? else: ?>
+                            <div class="btn btn-primary btn-sm"><?= GetMessage(
+                                    'CT_BNL_GOTO_DETAIL'
+                                ) ?></div>
+                        <? endif; ?>
                     </div>
                 </div>
             </div>
