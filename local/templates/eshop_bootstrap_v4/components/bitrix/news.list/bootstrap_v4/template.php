f4c06b144bd4b66b3686fae198522c05a2f37411
AlexeyVers <98410461+AlexeyVers@users.noreply.github.com>
2022-12-21 13:12:04 +0300

    102170 | Проерка для изображения и текста (#1)

--- local/templates/eshop_bootstrap_v4/components/bitrix/news.list/bootstrap_v4/template.php
+++ local/templates/eshop_bootstrap_v4/components/bitrix/news.list/bootstrap_v4/template.php
@@ -5,17 +5,23 @@
 <div class="col">
     <div class="row">
         <? foreach ($arResult['ITEMS'] as $arItem): ?>
-        <? $arProps = $arItem['DISPLAY_PROPERTIES']; ?>
+            <? $arProps = $arItem['DISPLAY_PROPERTIES'] ?>
             <div class="card normal-width">
-                <img src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" class="card-img-top"/>
+                <? if ($arItem['PREVIEW_PICTURE']): ?>
+                    <img src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" class="card-img-top"/>
+                <? endif; ?>
                 <div class="card-body">
                     <div class="card-title"><a
                                 href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a></div>
-                    <p class="card-text"><?= $arItem['PREVIEW_TEXT']; ?></p>
-                    <div class="news-list-view news-list-post-params">
-                        <span class="news-list-param"><?= $arProps['DATE_CREATE']['NAME'] ?>:</span>
-                        <span class="news-list-value"><?= $arProps['DATE_CREATE']['VALUE'] ?></span>
-                    </div>
+                    <? if ($arItem['PREVIEW_TEXT']): ?>
+                        <p class="card-text"><?= $arItem['PREVIEW_TEXT']; ?></p>
+                    <? endif; ?>
+                    <? if ($arProps['DATE_CREATE']['VALUE']): ?>
+                        <div class="news-list-view news-list-post-params">
+                            <span class="news-list-param"><?= $arProps['DATE_CREATE']['NAME'] ?>:</span>
+                            <span class="news-list-value"><?= $arProps['DATE_CREATE']['VALUE'] ?></span>
+                        </div>
+                    <? endif; ?>
                     <div class="news-list-more">
                         <a class="btn btn-primary btn-sm" href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= GetMessage(
                                 'CT_BNL_GOTO_DETAIL'
