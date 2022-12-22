e16cf78c8f1d4817da08752dcb246e73ce3a01ab
AlexeyVers <juk50075@gmail.com>
2022-12-22 11:20:59 +0300

    Доавбляет в слайдер провкрку выводв

--- local/templates/eshop_bootstrap_v4/components/bitrix/news.list/slider/template.php
+++ local/templates/eshop_bootstrap_v4/components/bitrix/news.list/slider/template.php
@@ -1,31 +1,20 @@
-<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
-$this->addExternalJS("https://code.jquery.com/jquery-3.6.2.slim.min.js");
 ?>
-<div class="col">
-    <div class="slider row">
-        <ul>
-            <? foreach ($arResult['ITEMS'] as $arItem): ?>
+<div class="slider">
+    <ul>
+        <? foreach ($arResult["ITEMS"] as $arItem): ?>
+            <? $arProperty = $arItem["DISPLAY_PROPERTIES"]['VIDNO_NOWOST']['ID']; ?>
+            <? if ($arProperty > 1): ?>
                 <li>
-                    <div class="card">
-                        <img src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" class="card-img-top"/>
-                        <div class="card-body">
-                            <div class="card-title"><a
-                                        href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= $arItem['NAME'] ?></a></div>
-                            <p class="card-text"><?= $arItem['PREVIEW_TEXT']; ?></p>
-                            <div class="news-list-more">
-                                <a class="btn btn-primary btn-sm"
-                                   href="<?= $arItem['DETAIL_PAGE_URL'] ?>"><?= GetMessage(
-                                        'CT_BNL_GOTO_DETAIL'
-                                    ) ?></a>
-                            </div>
-                        </div>
-                    </div>
+                    <img src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" class="card-img-top"/>
+                    <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><b><?= $arItem["NAME"] ?></b></a><br/>
+                    <?= $arItem["PREVIEW_TEXT"]; ?>
                 </li>
-            <? endforeach; ?>
-        </ul>
-    </div>
-    <?= $arResult['NAV_STRING'] ?>
+            <? endif; ?>
+        <? endforeach; ?>
+    </ul>
 </div>
-<script type="text/javascript" src="local/templates/eshop_bootstrap_v4/components/bitrix/news.list/slider/slider.js" async></script>
+<script type="text/javascript" src="local/templates/eshop_bootstrap_v4/components/bitrix/news.list/slider/slider.js"
+        async></script>
